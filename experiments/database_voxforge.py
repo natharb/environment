c0c2876ccf479a2fa6c1297b411022b5ebc68b71
Nathalia <nathbapt@decom.fee.unicamp.br>
2018-09-26 22:07:45 -0300

    adicionando novos protocolos

--- experiments/database_voxforge.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from bob.db.voxforge import Database
-
-voxforge_wav_directory = "./voxforge"
-
-database = Database(original_directory = voxforge_wav_directory, original_extension = ".wav")
