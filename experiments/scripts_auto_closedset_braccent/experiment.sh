a5b42481b556073dedfe6d2de02bda225291fe06
Nathalia <nathbapt@decom.fee.unicamp.br>
2018-09-29 16:36:08 -0300

    scripts de simulacao

---
+++ experiments/scripts_auto_closedset_braccent/experiment.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+../bin/verify.py config_GMM_256_fold1.py database_GMM_256_fold1.py
+rm -rf results/closedset_braccent/GMM/256/fold_1/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_1/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold2.py database_GMM_256_fold2.py
+rm -rf results/closedset_braccent/GMM/256/fold_2/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_2/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold3.py database_GMM_256_fold3.py
+rm -rf results/closedset_braccent/GMM/256/fold_3/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_3/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold4.py database_GMM_256_fold4.py
+rm -rf results/closedset_braccent/GMM/256/fold_4/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_4/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold5.py database_GMM_256_fold5.py
+rm -rf results/closedset_braccent/GMM/256/fold_5/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_5/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold6.py database_GMM_256_fold6.py
+rm -rf results/closedset_braccent/GMM/256/fold_6/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_6/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold7.py database_GMM_256_fold7.py
+rm -rf results/closedset_braccent/GMM/256/fold_7/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_7/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold8.py database_GMM_256_fold8.py
+rm -rf results/closedset_braccent/GMM/256/fold_8/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_8/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold9.py database_GMM_256_fold9.py
+rm -rf results/closedset_braccent/GMM/256/fold_9/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_9/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_256_fold10.py database_GMM_256_fold10.py
+rm -rf results/closedset_braccent/GMM/256/fold_10/temp/subdirectory/extracted/ results/closedset_braccent/GMM/256/fold_10/temp/subdirectory/preprocessed/
+
+../bin/verify.py config_GMM_512_fold1.py database_GMM_512_fold1.py
+rm -rf results/closedset_braccent/GMM/512/fold_1/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_1/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold2.py database_GMM_512_fold2.py
+rm -rf results/closedset_braccent/GMM/512/fold_2/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_2/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold3.py database_GMM_512_fold3.py
+rm -rf results/closedset_braccent/GMM/512/fold_3/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_3/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold4.py database_GMM_512_fold4.py
+rm -rf results/closedset_braccent/GMM/512/fold_4/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_4/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold5.py database_GMM_512_fold5.py
+rm -rf results/closedset_braccent/GMM/512/fold_5/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_5/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold6.py database_GMM_512_fold6.py
+rm -rf results/closedset_braccent/GMM/512/fold_6/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_6/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold7.py database_GMM_512_fold7.py
+rm -rf results/closedset_braccent/GMM/512/fold_7/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_7/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold8.py database_GMM_512_fold8.py
+rm -rf results/closedset_braccent/GMM/512/fold_8/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_8/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold9.py database_GMM_512_fold9.py
+rm -rf results/closedset_braccent/GMM/512/fold_9/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_9/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_512_fold10.py database_GMM_512_fold10.py
+rm -rf results/closedset_braccent/GMM/512/fold_10/temp/subdirectory/extracted/ results/closedset_braccent/GMM/512/fold_10/temp/subdirectory/preprocessed/
+
+../bin/verify.py config_GMM_1024_fold1.py database_GMM_1024_fold1.py
+rm -rf results/closedset_braccent/GMM/1024/fold_1/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_1/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold2.py database_GMM_1024_fold2.py
+rm -rf results/closedset_braccent/GMM/1024/fold_2/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_2/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold3.py database_GMM_1024_fold3.py
+rm -rf results/closedset_braccent/GMM/1024/fold_3/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_3/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold4.py database_GMM_1024_fold4.py
+rm -rf results/closedset_braccent/GMM/1024/fold_4/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_4/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold5.py database_GMM_1024_fold5.py
+rm -rf results/closedset_braccent/GMM/1024/fold_5/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_5/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold6.py database_GMM_1024_fold6.py
+rm -rf results/closedset_braccent/GMM/1024/fold_6/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_6/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold7.py database_GMM_1024_fold7.py
+rm -rf results/closedset_braccent/GMM/1024/fold_7/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_7/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold8.py database_GMM_1024_fold8.py
+rm -rf results/closedset_braccent/GMM/1024/fold_8/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_8/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold9.py database_GMM_1024_fold9.py
+rm -rf results/closedset_braccent/GMM/1024/fold_9/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_9/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_1024_fold10.py database_GMM_1024_fold10.py
+rm -rf results/closedset_braccent/GMM/1024/fold_10/temp/subdirectory/extracted/ results/closedset_braccent/GMM/1024/fold_10/temp/subdirectory/preprocessed/
+
+../bin/verify.py config_GMM_2048_fold1.py database_GMM_2048_fold1.py
+rm -rf results/closedset_braccent/GMM/2048/fold_1/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_1/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold2.py database_GMM_2048_fold2.py
+rm -rf results/closedset_braccent/GMM/2048/fold_2/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_2/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold3.py database_GMM_2048_fold3.py
+rm -rf results/closedset_braccent/GMM/2048/fold_3/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_3/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold4.py database_GMM_2048_fold4.py
+rm -rf results/closedset_braccent/GMM/2048/fold_4/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_4/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold5.py database_GMM_2048_fold5.py
+rm -rf results/closedset_braccent/GMM/2048/fold_5/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_5/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold6.py database_GMM_2048_fold6.py
+rm -rf results/closedset_braccent/GMM/2048/fold_6/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_6/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold7.py database_GMM_2048_fold7.py
+rm -rf results/closedset_braccent/GMM/2048/fold_7/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_7/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold8.py database_GMM_2048_fold8.py
+rm -rf results/closedset_braccent/GMM/2048/fold_8/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_8/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold9.py database_GMM_2048_fold9.py
+rm -rf results/closedset_braccent/GMM/2048/fold_9/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_9/temp/subdirectory/preprocessed/
+../bin/verify.py config_GMM_2048_fold10.py database_GMM_2048_fold10.py
+rm -rf results/closedset_braccent/GMM/2048/fold_10/temp/subdirectory/extracted/ results/closedset_braccent/GMM/2048/fold_10/temp/subdirectory/preprocessed/
+
