5ae246fdc8504c07b148c553518c815d4551764b
Nathalia <nathbapt@decom.fee.unicamp.br>
2018-09-30 19:11:12 -0300

    novos scripts

---
+++ experiments/scripts_auto_closedset_ynoguti/config_GMM_2048_fold2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+#Nathália Alves Rocha Batista (nathbapt@decom.fee.unicamp.br)
+
+import sys
+sys.path.insert(0, '.')
+
+import bob.bio.spear
+import bob.bio.gmm
+import numpy
+import scipy.spatial
+
+temp_directory = './results/closedset_ynoguti/GMM/2048/fold_2/temp/'
+result_directory = './results/closedset_ynoguti/GMM/2048/fold_2/results/'
+sub_directory = 'subdirectory'
+
+database = 'database_GMM_2048_fold2.py'
+
+groups = ['dev']
+#groups = ['dev', 'eval']
+
+preprocessor = bob.bio.spear.preprocessor.Energy_2Gauss(max_iterations = 10, convergence_threshold = 0.0005, variance_threshold = 0.0005,  win_length_ms = 20., win_shift_ms = 10., smoothing_window = 10)
+
+extractor = bob.bio.spear.extractor.Cepstral(win_length_ms = 25, win_shift_ms = 10, n_filters = 24 , dct_norm = False, f_min = 0, f_max = 4000, delta_win = 2, mel_scale = True, with_energy = True, with_delta = True, with_delta_delta = True, n_ceps = 19, pre_emphasis_coef = 0.97)
+
+algorithm = bob.bio.gmm.algorithm.GMMRegular(number_of_gaussians = 2048, kmeans_training_iterations = 10, gmm_training_iterations = 10,
+training_threshold = 5e-4, variance_threshold = 5e-4, update_weights = True, update_means = True, update_variances = True, relevance_factor = 4, gmm_enroll_iterations = 1, responsibility_threshold = 0, INIT_SEED = 5489)
+
+parallel = 8
+verbose = 2
