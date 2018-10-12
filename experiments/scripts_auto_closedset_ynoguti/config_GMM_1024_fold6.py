45ec2e0faea87691d1ad3b56a446b3ae3755b9fd
Nathalia <nathbapt@decom.fee.unicamp.br>
2018-10-12 10:50:06 -0300

    atualizando ynogutti scripts

--- experiments/scripts_auto_closedset_ynoguti/config_GMM_1024_fold6.py
+++ experiments/scripts_auto_closedset_ynoguti/config_GMM_1024_fold6.py
@@ -26,5 +26,5 @@ extractor = bob.bio.spear.extractor.Cepstral(win_length_ms = 25, win_shift_ms = 
 algorithm = bob.bio.gmm.algorithm.GMMRegular(number_of_gaussians = 1024, kmeans_training_iterations = 10, gmm_training_iterations = 10,
 training_threshold = 5e-4, variance_threshold = 5e-4, update_weights = True, update_means = True, update_variances = True, relevance_factor = 4, gmm_enroll_iterations = 1, responsibility_threshold = 0, INIT_SEED = 5489)
 
-parallel = 8
+parallel = 40
 verbose = 2
