896d5a86acc7780404973ffe69d0bef908082e9b
Nathalia Baptista <nathalia@brava>
2018-10-16 12:15:35 -0300

    script updated

--- experiments/scripts_auto_crossdataset_ynoguti_cfpb/experiment.sh
+++ experiments/scripts_auto_crossdataset_ynoguti_cfpb/experiment.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 ../../bin/verify.py config_GMM_256_crossdataset.py database_GMM_256_crossdataset.py
-
-../../bin/verify.py --preprocessed_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/preprocessed/ --extracted_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/extracted/ config_GMM_512_crossdataset.py database_GMM_512_crossdataset.py
-
-../../bin/verify.py --preprocessed_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/preprocessed/ --extracted_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/extracted/ config_GMM_1024_crossdataset.py database_GMM_1024_crossdataset.py
-
-../../bin/verify.py --preprocessed_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/preprocessed/ --extracted_directory results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/extracted/ config_GMM_2048_crossdataset.py database_GMM_2048_crossdataset.py
-
 rm -rf results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/extracted/ results/crossdataset_ynoguti_cfpb/GMM/256/temp/subdirectory/preprocessed/
+
+../../bin/verify.py config_GMM_512_crossdataset.py database_GMM_512_crossdataset.py
 rm -rf results/crossdataset_ynoguti_cfpb/GMM/512/temp/subdirectory/extracted/ results/crossdataset_ynoguti_cfpb/GMM/512/temp/subdirectory/preprocessed/
+
+../../bin/verify.py config_GMM_1024_crossdataset.py database_GMM_1024_crossdataset.py
 rm -rf results/crossdataset_ynoguti_cfpb/GMM/1024/temp/subdirectory/extracted/ results/crossdataset_ynoguti_cfpb/GMM/1024/temp/subdirectory/preprocessed/
+
+../../bin/verify.py config_GMM_2048_crossdataset.py database_GMM_2048_crossdataset.py
 rm -rf results/crossdataset_ynoguti_cfpb/GMM/2048/temp/subdirectory/extracted/ results/crossdataset_ynoguti_cfpb/GMM/2048/temp/subdirectory/preprocessed/
 
