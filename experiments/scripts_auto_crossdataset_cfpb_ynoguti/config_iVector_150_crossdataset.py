ebd8d571ec9af718aaacd12436df332b71859f1f
Nathalia Baptista <nathalia@brava>
2018-10-26 12:59:11 -0200

    atualizando scripts

--- experiments/scripts_auto_crossdataset_cfpb_ynoguti/config_iVector_150_crossdataset.py
+++ experiments/scripts_auto_crossdataset_cfpb_ynoguti/config_iVector_150_crossdataset.py
@@ -10,11 +10,11 @@ import bob.bio.gmm
 import numpy
 import scipy.spatial
 
-temp_directory = './results/crossdataset_cfpb_braccent/iVector/200/temp/'
-result_directory = './results/crossdataset_cfpb_braccent/iVector/200/results/'
+temp_directory = './results/crossdataset_cfpb_ynoguti/iVector/150/temp/'
+result_directory = './results/crossdataset_cfpb_ynoguti/iVector/150/results/'
 sub_directory = 'subdirectory'
 
-database = 'database_iVector_200_crossdataset.py'
+database = 'database_iVector_150_crossdataset.py'
 
 groups = ['dev']
 #groups = ['dev', 'eval']
@@ -23,7 +23,7 @@ preprocessor = bob.bio.spear.preprocessor.Energy_2Gauss(max_iterations = 10, con
 
 extractor = bob.bio.spear.extractor.Cepstral(win_length_ms = 25, win_shift_ms = 10, n_filters = 24 , dct_norm = False, f_min = 0, f_max = 4000, delta_win = 2, mel_scale = True, with_energy = True, with_delta = True, with_delta_delta = True, n_ceps = 19, pre_emphasis_coef = 0.97)
 
-algorithm = bob.bio.gmm.algorithm.IVector(subspace_dimension_of_t = 200, tv_training_iterations = 10, update_sigma = True, use_whitening = True, use_lda = False, use_wccn = False, use_plda = False, lda_dim = 50, plda_dim_F  = 50, plda_dim_G = 50, plda_training_iterations = 50, number_of_gaussians = 2048)
+algorithm = bob.bio.gmm.algorithm.IVector(subspace_dimension_of_t = 150, tv_training_iterations = 10, update_sigma = True, use_whitening = True, use_lda = False, use_wccn = False, use_plda = False, lda_dim = 50, plda_dim_F  = 50, plda_dim_G = 50, plda_training_iterations = 50, number_of_gaussians = 2048)
 
-parallel = 4
+parallel = 8
 verbose = 2
--- experiments/scripts_auto_crossdataset_cfpb_ynoguti/experiment_iVector.sh
+++ experiments/scripts_auto_crossdataset_cfpb_ynoguti/experiment_iVector.sh
@@ -3,8 +3,8 @@
 ../../bin/verify.py config_iVector_200_crossdataset.py database_iVector_200_crossdataset.py
 rm -rf results/crossdataset_cfpb_ynoguti/iVector/200/temp/subdirectory/extracted/ results/crossdataset_cfpb_ynoguti/iVector/200/temp/subdirectory/preprocessed/
 
-../../bin/verify.py config_iVector_300_crossdataset.py database_iVector_300_crossdataset.py
-rm -rf results/crossdataset_cfpb_ynoguti/iVector/300/temp/subdirectory/extracted/ results/crossdataset_cfpb_ynoguti/iVector/300/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_150_crossdataset.py database_iVector_150_crossdataset.py
+rm -rf results/crossdataset_cfpb_ynoguti/iVector/150/temp/subdirectory/extracted/ results/crossdataset_cfpb_ynoguti/iVector/150/temp/subdirectory/preprocessed/
 
-../../bin/verify.py config_iVector_400_crossdataset.py database_iVector_400_crossdataset.py
-rm -rf results/crossdataset_cfpb_ynoguti/iVector/400/temp/subdirectory/extracted/ results/crossdataset_cfpb_ynoguti/iVector/400/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_100_crossdataset.py database_iVector_100_crossdataset.py
+rm -rf results/crossdataset_cfpb_ynoguti/iVector/100/temp/subdirectory/extracted/ results/crossdataset_cfpb_ynoguti/iVector/100/temp/subdirectory/preprocessed/
