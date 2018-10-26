d5a3fcc1db736df17d84bac75ec1d91ec36ca6a2
Nathalia Batista <nathbapt@dspcom-wkst-1.fee.unicamp.br>
2018-10-26 11:40:14 -0300

    mudando parametros dosexperimentos iVector

--- experiments/scripts_auto_closedset_cfpb/experiment_iVector.sh
+++ experiments/scripts_auto_closedset_cfpb/experiment_iVector.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-../../bin/verify.py config_iVector_200_fold1.py database_iVector_200_fold1.py
-rm -rf results/closedset_cfpb/iVector/200/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/200/fold_1/temp/subdirectory/preprocessed/
-../../bin/verify.py config_iVector_200_fold2.py database_iVector_200_fold2.py
-rm -rf results/closedset_cfpb/iVector/200/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/200/fold_2/temp/subdirectory/preprocessed/
+#../../bin/verify.py config_iVector_200_fold1.py database_iVector_200_fold1.py
+#rm -rf results/closedset_cfpb/iVector/200/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/200/fold_1/temp/subdirectory/preprocessed/
+#../../bin/verify.py config_iVector_200_fold2.py database_iVector_200_fold2.py
+#rm -rf results/closedset_cfpb/iVector/200/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/200/fold_2/temp/subdirectory/preprocessed/
 
-../../bin/verify.py config_iVector_300_fold1.py database_iVector_300_fold1.py
-rm -rf results/closedset_cfpb/iVector/300/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/300/fold_1/temp/subdirectory/preprocessed/
-../../bin/verify.py config_iVector_300_fold2.py database_iVector_300_fold2.py
-rm -rf results/closedset_cfpb/iVector/300/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/300/fold_2/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_150_fold1.py database_iVector_150_fold1.py
+rm -rf results/closedset_cfpb/iVector/150/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/150/fold_1/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_150_fold2.py database_iVector_300_fold2.py
+rm -rf results/closedset_cfpb/iVector/150/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/150/fold_2/temp/subdirectory/preprocessed/
 
-../../bin/verify.py config_iVector_400_fold1.py database_iVector_400_fold1.py
-rm -rf results/closedset_cfpb/iVector/400/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/400/fold_1/temp/subdirectory/preprocessed/
-../../bin/verify.py config_iVector_400_fold2.py database_iVector_400_fold2.py
-rm -rf results/closedset_cfpb/iVector/400/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/400/fold_2/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_100_fold1.py database_iVector_100_fold1.py
+rm -rf results/closedset_cfpb/iVector/100/fold_1/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/100/fold_1/temp/subdirectory/preprocessed/
+../../bin/verify.py config_iVector_100_fold2.py database_iVector_400_fold2.py
+rm -rf results/closedset_cfpb/iVector/100/fold_2/temp/subdirectory/extracted/ results/closedset_cfpb/iVector/100/fold_2/temp/subdirectory/preprocessed/
